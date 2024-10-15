63fbfd59e9edef02cc8dcf34e55add3717542338
Alexander Soudry <soudryalex@gmail.com>
2024-10-15 16:56:04 -0400

    added login controller endpoint:

---
+++ src/main/java/com/soudry/expense_reimbursement/controller/LoginController.java
@@ -0,0 +1,30 @@
+package com.soudry.expense_reimbursement.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.soudry.expense_reimbursement.DTO.LoginRequest;
+import com.soudry.expense_reimbursement.DTO.LoginResponse;
+import com.soudry.expense_reimbursement.services.LoginService;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @Autowired
+    public LoginController (LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        LoginResponse response = loginService.login(request);
+        return ResponseEntity.ok(response);
+    }
+}
