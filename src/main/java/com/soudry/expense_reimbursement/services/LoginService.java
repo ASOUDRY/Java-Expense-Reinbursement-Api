51d4bb06e9d0be0a106346033595b0537be45f6f
Alexander Soudry <soudryalex@gmail.com>
2024-10-21 13:48:47 -0400

    removes password validation logic and leaves it up to Spring Data. Added and altered methods to generate and return jwt to pass back to the controller

--- src/main/java/com/soudry/expense_reimbursement/services/LoginService.java
+++ src/main/java/com/soudry/expense_reimbursement/services/LoginService.java
@@ -1,28 +1,50 @@
 package com.soudry.expense_reimbursement.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.stereotype.Service;
 
 import com.soudry.expense_reimbursement.DTO.Request.LoginRequest;
 import com.soudry.expense_reimbursement.DTO.Response.LoginResponse;
 import com.soudry.expense_reimbursement.entities.User;
 import com.soudry.expense_reimbursement.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import com.soudry.expense_reimbursement.util.JwtUtil;
 
 @Service
 public class LoginService {
 
     private final UserRepository userRepo;
+    private final AuthenticationManager authenticationManager;
+    private final UserToSpringUserService userToSpringUserService;
+    private final JwtUtil jwtUtil;
 
     @Autowired
-    public LoginService (UserRepository userRepo) {
+    public LoginService (UserRepository userRepo, 
+    AuthenticationManager authenticationManager, 
+    UserToSpringUserService userToSpringUserService, 
+    JwtUtil jwtUtil
+    ) {
         this.userRepo = userRepo;
+        this.authenticationManager = authenticationManager;
+        this.userToSpringUserService = userToSpringUserService;
+        this.jwtUtil = jwtUtil;
     }
-
 
     public LoginResponse login(LoginRequest request) {
-        User user = userRepo.findByUsernameAndPassword(request.getUsername(), request.getPassword());
-        LoginResponse response = new LoginResponse(user.getUsername(), user.getPassword(), user.getEmail());
+        // User user = userRepo.findByUsernameAndPassword(request.getUsername(), request.getPassword());
+        String jwt = this.authenticationLogic(request.getUsername(), request.getPassword());
+        User user = userRepo.findByUsername(request.getUsername());
+        LoginResponse response = new LoginResponse(user.getUsername(), user.getPassword(), user.getEmail(), jwt);
         return response;
     }
-    
+
+    public String authenticationLogic(String username, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        final UserDetails userDetails = userToSpringUserService.loadUserByUsername(username);
+        final String jwt = jwtUtil.generateToken(userDetails);
+        return jwt;
+    }
 }
