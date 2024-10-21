75791601f131232e3192f6ef1aba4be48195ec14
Alexander Soudry <soudryalex@gmail.com>
2024-10-21 13:40:43 -0400

    Added security config file which Spring Security uses to manage the relevant beans for authentication and authorization

---
+++ src/main/java/com/soudry/expense_reimbursement/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.soudry.expense_reimbursement.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.soudry.expense_reimbursement.services.UserToSpringUserService;
+import com.soudry.expense_reimbursement.util.JwtUtil;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {   
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    private UserToSpringUserService userToSpringUserService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+        }
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http.csrf(csrf -> 
+                        csrf.disable())
+                .authorizeHttpRequests((authorizeHttpRequests) ->
+                                authorizeHttpRequests
+                                        .requestMatchers("/login/**", "register/**").permitAll()
+                                        .requestMatchers("/ticket/**").authenticated()
+                        )
+                        .sessionManagement((sessionManagment) -> sessionManagment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        ;
+                          http.addFilterBefore(jwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
+                return http.build();
+        }
+
+        @Bean
+        public JwtRequestFilter jwtRequestFilter() {
+            return new JwtRequestFilter(jwtUtil, userToSpringUserService);
+        }
+    }
