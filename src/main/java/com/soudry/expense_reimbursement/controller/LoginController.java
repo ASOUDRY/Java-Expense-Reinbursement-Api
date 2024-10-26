94c43414f4ec7ce2579886ee0f4143395c9fd5cf
Alexander Soudry <soudryalex@gmail.com>
2024-10-25 23:45:43 -0400

    refactored controllers to throw exceptions propagated from the service layer

--- src/main/java/com/soudry/expense_reimbursement/controller/LoginController.java
+++ src/main/java/com/soudry/expense_reimbursement/controller/LoginController.java
@@ -1,7 +1,11 @@
 package com.soudry.expense_reimbursement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.soudry.expense_reimbursement.DTO.Request.LoginRequest;
 import com.soudry.expense_reimbursement.DTO.Response.LoginResponse;
+import com.soudry.expense_reimbursement.Exceptions.ErrorResponse;
 import com.soudry.expense_reimbursement.services.LoginService;
 
 @RestController
@@ -21,10 +26,23 @@ public class LoginController {
     public LoginController (LoginService loginService) {
         this.loginService = loginService;
     }
+    
+    @GetMapping("/Test")
+    public String foo() {
+        return "Test";
+    }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
-        LoginResponse response = loginService.login(request);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        try {
+            LoginResponse response = loginService.login(request);
+            return ResponseEntity.ok(response);
+        } catch (BadCredentialsException e) {
+           return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                             .body(new ErrorResponse("Invalid username or password", HttpStatus.UNAUTHORIZED.value()));
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                .body(new ErrorResponse("User not found", HttpStatus.NOT_FOUND.value()));
+        }  
     }
 }
--- src/main/java/com/soudry/expense_reimbursement/controller/RegistrationController.java
+++ src/main/java/com/soudry/expense_reimbursement/controller/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.soudry.expense_reimbursement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.soudry.expense_reimbursement.DTO.Request.RegistrationRequest;
 import com.soudry.expense_reimbursement.DTO.Response.RegistrationResponse;
+import com.soudry.expense_reimbursement.Exceptions.AccountAlreadyExistsException;
+import com.soudry.expense_reimbursement.Exceptions.ErrorResponse;
+import com.soudry.expense_reimbursement.Exceptions.PasswordsDoNotMatchException;
 import com.soudry.expense_reimbursement.services.RegistrationService;
 
 @RestController
@@ -23,10 +27,20 @@ public class RegistrationController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<RegistrationResponse> registration(@RequestBody RegistrationRequest request) {
-        RegistrationResponse response = registrationService.register(request.getUsername(), request.getPassword(), 
-        request.getConfirmedPassword(), request.getEmail());
-        // RegistrationResponse response = new RegistrationResponse();
+    public ResponseEntity<?> registration(@RequestBody RegistrationRequest request) {
+        try {
+        RegistrationResponse response = registrationService.register(request);
         return ResponseEntity.ok(response);
+        } catch (AccountAlreadyExistsException e) {
+            return ResponseEntity.status(409).body(
+                new ErrorResponse("Account already exists", 
+                HttpStatus.CONFLICT.value())
+            );
+        } catch (PasswordsDoNotMatchException e) {
+            return ResponseEntity.status(400).body(
+                new ErrorResponse("Passwords do not match", 
+                HttpStatus.BAD_REQUEST.value())
+            );
+        }
     }
 }
