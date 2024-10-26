3abd3df478a74be942245d56185b68e726fa2af1
Alexander Soudry <soudryalex@gmail.com>
2024-10-25 23:49:30 -0400

    added logic to check if an account with that username already exists as well as having the method throw run time exceptions instead of returning null

--- src/main/java/com/soudry/expense_reimbursement/services/RegistrationService.java
+++ src/main/java/com/soudry/expense_reimbursement/services/RegistrationService.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.soudry.expense_reimbursement.DTO.Request.RegistrationRequest;
 import com.soudry.expense_reimbursement.DTO.Response.RegistrationResponse;
+import com.soudry.expense_reimbursement.Exceptions.AccountAlreadyExistsException;
+import com.soudry.expense_reimbursement.Exceptions.PasswordsDoNotMatchException;
 import com.soudry.expense_reimbursement.entities.User;
 import com.soudry.expense_reimbursement.repository.UserRepository;
 
@@ -20,13 +23,27 @@ public class RegistrationService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public RegistrationResponse register(String username, String password, String confirmedPassword, String email) {
-        if (password.equals(confirmedPassword)) {
-            String encodedPassword = passwordEncoder.encode(confirmedPassword);
-            User newUser = new User(username, encodedPassword, email);
-            userRepo.save(newUser);
-            return new RegistrationResponse(username, encodedPassword, email);
+    public RegistrationResponse register(RegistrationRequest request) {
+        String username = request.getUsername();
+        String password = request.getPassword();
+        String comfirmedPassword = request.getConfirmedPassword();
+        String email = request.getEmail();
+        // Check if passwords match
+        if (!password.matches(comfirmedPassword)) {
+            throw new PasswordsDoNotMatchException("Passwords do not match");
         }
-        return null;
+
+        // Check if the account already exists
+        if (userRepo.findById(username).isPresent()) {
+            throw new AccountAlreadyExistsException("Account already exists");
+        }
+
+        // Encode the password and create a new user
+        String encodedPassword = passwordEncoder.encode(password);
+        User newUser = new User(username, encodedPassword, email);
+        userRepo.save(newUser);
+        
+        // Return the response indicating successful registration
+        return new RegistrationResponse(username, encodedPassword, email);
     }
 }
